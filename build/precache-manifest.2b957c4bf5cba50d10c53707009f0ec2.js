4a3e69c7ab3a7581630ab22f40e0238b024dd5e4
htmlpages2020 <65441667+htmlpages2020@users.noreply.github.com>
2020-06-08 11:38:14 +0530

    Add files via upload

---
+++ build/precache-manifest.2b957c4bf5cba50d10c53707009f0ec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8714c7460947e6d6c1c4a4ebb8fb5c3b",
+    "url": "/learn/index.html"
+  },
+  {
+    "revision": "82db2217a207755185e9",
+    "url": "/learn/static/css/2.7ad897a5.chunk.css"
+  },
+  {
+    "revision": "91c462bffdf474585d67",
+    "url": "/learn/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82db2217a207755185e9",
+    "url": "/learn/static/js/2.b98b5679.chunk.js"
+  },
+  {
+    "revision": "890b83c3b503af9a5630afd9ff112890",
+    "url": "/learn/static/js/2.b98b5679.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91c462bffdf474585d67",
+    "url": "/learn/static/js/main.e5466d11.chunk.js"
+  },
+  {
+    "revision": "ba1497393eaa5de9ee16",
+    "url": "/learn/static/js/runtime-main.d9e38610.js"
+  }
+]);
